352d400ee8192f7e1e37498e6f92be84a772835c
鈴木敦 <>
2019-03-21 20:02:48 +0900

    tab1の修正

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+
+class Comment {
+  name: string;
+  message: string;
+}
+
+const COMMENTS: Comment[] = [
+  { name: '山本 剛史', message: 'お疲れ様です！' },
+  { name: '鈴木 敦', message: 'この前の件ですが、どうなりましたか？' },
+];
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  comments = COMMENTS;
+
+
+  onClickedSend(){
+      // console.log(index);
+
+}
+}
